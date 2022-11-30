2bd43efeb13ae3fe5c6dc0664a80c35e8d635e0c
ApaBird <apa.birds@mail.ru>
2022-11-30 12:59:00 +0800

    Revert "Revert "CameraControler+Selected(bug in Selected)""
    This reverts commit 4b445ab467465b5b9e60da3c68a87ad197f00c5a.

--- BKR/Assets/Scripts/Select.cs
+++ BKR/Assets/Scripts/Select.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Select : MonoBehaviour
 {
-    private List<GameObject> selectedObject = new List<GameObject>();
+    [SerializeField] private List<GameObject> selectedObject = new List<GameObject>();
     [SerializeField] private Camera camera;
 
     public void selectCamera(Camera c) => this.camera = c;
@@ -12,15 +12,30 @@ public class Select : MonoBehaviour
     void Update()
     {
         Vector3 mousePosition = camera.ScreenPointToRay(Input.mousePosition).GetPoint(camera.transform.position.z);
-        this.transform.localScale = new Vector3( mousePosition.x - this.transform.position.x, this.transform.position.y - mousePosition.y, 0);
+        float sizeX = 0.1f;
+        float sizeY = 0.1f;
+        if (Mathf.Abs(mousePosition.x - this.transform.position.x) > 0.1)
+            sizeX = mousePosition.x - this.transform.position.x;
+        if (Mathf.Abs(mousePosition.y - this.transform.position.y) > 0.1)
+            sizeY = mousePosition.y - this.transform.position.y;
+        this.transform.localScale = new Vector3( sizeX, sizeY, 0);
     }
 
-    public List<GameObject> GetListObject() => selectedObject;
+    public GameObject[] GetObjects()
+    {
+        GameObject[] x = new GameObject[selectedObject.Count];
+        for (int i = 0; i < selectedObject.Count; i++)
+            x[i] = selectedObject[i];
+        return x;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Component>())
+        {
+            collision.GetComponent<Component>().Select();
             selectedObject.Add(collision.gameObject);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -28,6 +43,7 @@ public class Select : MonoBehaviour
         for (int i = 0; i < selectedObject.Count; i++)
             if (selectedObject[i] == collision.gameObject)
             {
+                selectedObject[i].GetComponent<Component>().Unselect();
                 selectedObject.RemoveAt(i);
                 break;
             }
